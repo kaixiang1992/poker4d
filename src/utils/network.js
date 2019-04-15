44357088de0390c4ad6c280238476e6957ef1c5b
kaixiang <wang-kaixiang@outlook.com>
2019-04-15 22:55:14 +0800

    更改network

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,11 +1,11 @@
 const config = {
-    contractName: process.env.NODE_ENV != 'production' ? 'poker4dtgame' : 'poker4dtgame',
+    contractName: process.env.NODE_ENV == 'production' ? 'poker4dtgame' : 'poker4dtgame',
     network: {
         blockchain: 'eos',
-        host: process.env.NODE_ENV != 'production' ? 'api1.eosasia.one' : '//api-kylin.eosasia.one',
+        host: process.env.NODE_ENV == 'production' ? 'api1.eosasia.one' : '//api-kylin.eosasia.one',
         port: 443, 
         protocol: 'https',
-        chainId: process.env.NODE_ENV != 'production' ? 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906' : '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191' 
+        chainId: process.env.NODE_ENV == 'production' ? 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906' : '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191' 
     }
 }
 
